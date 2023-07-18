8aa53deaf5ec50427dffb86b561db0d3bf8fd6ca
KoshikawaShinya <satowimp0319@gmail.com>
2023-07-19 03:23:59 +0900

    add show detail.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,16 @@ class PostController extends Controller
         // withを使うことでviewに変数を渡す
         return view('posts.index')->with(['posts' => $post->getPaginateBylimit()]);
     }
+    
+    // 暗黙の結合(ルートパラメータと関数の引数の名前が同じ)
+    /**
+    * 特定IDのpostを表示する
+    *
+    * @params Object Post // 引数の$postはidを指定したのPostインスタンス
+    * @return Reposnse post view
+    */
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
 }
