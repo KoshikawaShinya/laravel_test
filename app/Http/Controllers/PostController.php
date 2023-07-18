6a66f1f3515347ab4c4d4a8b20f864be02b2751d
KoshikawaShinya <satowimp0319@gmail.com>
2023-07-19 01:58:30 +0900

    add paginator

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,7 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(Post $post){
-        $test = $post->orderBy('updated_at', 'DESC')->limit(2)->toSql(); //確認用に追加
-        dd($test);
         // withを使うことでviewに変数を渡す
-        return view('posts.index')->with(['posts' => $post->getByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateBylimit()]);
     }
 }
